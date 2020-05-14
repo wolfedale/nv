9fae7b437b7d62e37fc9c5d2365a060ce8e47003
Pawel Grzesik <pgrzesik@ebay.com>
2020-05-14 15:57:15 +0200

    Struct for tasks

--- pkg/tasks/tasks.go
+++ pkg/tasks/tasks.go
@@ -1,7 +1,31 @@
 package tasks
 
-type Tasks struct{}
+type TaskInFace interface {
+	Create() error
+	Edit() error
+	Delete() error
+}
+
+type Tasks struct {
+	Task []Task
+}
+
+type Task struct {
+	Name string
+}
 
 func New() (*Tasks, error) {
 	return &Tasks{}, nil
+}
+
+func (t *Tasks) Create() error {
+	return nil
+}
+
+func (t *Tasks) Edit() error {
+	return nil
+}
+
+func (t *Tasks) Delete() error {
+	return nil
 }
